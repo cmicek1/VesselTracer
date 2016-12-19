bfb6f6048e7d119f27825df75ca9b01afd23352a
Chris Micek <cmicek325@verizon.net>
2016-12-19 01:23:43 -0500

    Parts that take forever commented out; connected-component labeling for intersections added back in; plots (currently incorrect) vessel tracing for each vessel found (hard coded for two reasonably prioritized candidates for now)

--- Segment.m
+++ Segment.m
@@ -13,7 +13,7 @@ classdef Segment
         mu
         
         % Scale (x, y, z)
-        rho
+        sigma
         
         % Orientation (z, y, x)
         phi
@@ -27,7 +27,11 @@ classdef Segment
         % For Jacobian calculation
         G
         
-        % Background and foreground image intensities
+        % Background and foreground image intensities (original estimates)
+        IF0
+        IB0
+        
+        % Background and foreground image intensities (updated)
         IF
         IB
         
@@ -36,28 +40,35 @@ classdef Segment
         
         % Max iterations for initialization
         initIterMax
+        
+        % Log-likelihood
+        L
     end
     methods (Access = protected)
         % Marginal distribution for Laplacian noise
         function val = f(obj, arg)
-            val = obj.alpha / 2 * exp(-obj.alpha * abs(double(arg)));
+            val = obj.alpha / 2 .* exp(-obj.alpha .* abs(double(arg)));
         end
         
         function J = Jacobian(obj, start, point)
             d_mu = eye(3);
             d_sigma = zeros(3, 3);
-            d_phi = zeros(3, 3);
+            d_q = zeros(3, 4);
             d_epsilon = zeros(3, 1);
             if ~start
                 sig = calcEllipse(point(1), point(2), point(3), obj.epsilon);
-                u = point / norm(point);
+                u = point' / norm(point);
                 qR = quat2rotm(obj.q);
                 d_sigma = qR * diag(sig * u);
-                d_phi = -qR * diag(obj.sigma) * sig * u * obj.G;
+                p_vec = diag(obj.sigma) * sig * u;
+                P = [0, -p_vec(3), p_vec(2);
+                    p_vec(3), 0, -p_vec(1);
+                    -p_vec(2), p_vec(1), 0];
+                d_q = -qR * P * obj.G;
                 d_epsilon = qR * diag(obj.sigma) * calc_dsig(point(1),...
                     point(2), point(3), obj.epsilon) * u;
             end
-            J = [d_mu, d_sigma, d_phi, d_epsilon];
+            J = [d_mu, d_sigma, d_q, d_epsilon];
         end
         function obj = updateG(obj)
             obj.G = 2 * [-obj.q(2), obj.q(1), obj.q(3), -obj.q(4);
@@ -67,31 +78,47 @@ classdef Segment
     end
     methods
         
-        function obj = Segment()
+        function obj = Segment(varargin)
+            if nargin > 0
+                obj.x = varargin{1};
+                obj.y = varargin{2};
+                obj.z = varargin{3};
+            end
             obj.epsilon = 0.25;
             obj.mu = [0; 0; 0];
-            obj.rho = [3; 3; 4.5];
+            obj.sigma = [3; 3; 1.5];
             obj.phi = [0; 0; 0];
             obj.R = eye(3);
             obj.q = [1, 0, 0, 0];
             obj = obj.updateG();
-            obj.IF = 0;
-            obj.IB = 255;
+            obj.IF0 = 0;
+            obj.IB0 = 255;
             obj.alpha = 0;
-            obj.initIterMax = 0;
+            obj.initIterMax = 20;
         end
         
-        function obj = translate(obj, point)
+        function obj = translate(obj, point, imSz)
             obj.mu = obj.mu + point;
+            obj.mu(1) = obj.mu(1) * (obj.mu(1) >= 1 && obj.mu(1) <= imSz(1)) + ...
+                (obj.mu(1) < 1) + (obj.mu(1) > imSz(1)) * imSz(1);
+            obj.mu(2) = obj.mu(2) * (obj.mu(2) >= 1 && obj.mu(2) <= imSz(2)) + ...
+                (obj.mu(2) < 1) + (obj.mu(2) > imSz(2)) * imSz(2);
+            obj.mu(3) = obj.mu(3) * (obj.mu(3) >= 1 && obj.mu(3) <= imSz(3)) + ...
+                (obj.mu(3) < 1) + (obj.mu(3) > imSz(3)) * imSz(3);
             obj.x = obj.x + point(1);
             obj.y = obj.y + point(2);
             obj.z = obj.z + point(3);
         end
         
         function obj = rotate(obj, angle)
-            obj.phi = mod(obj.phi + angle, 2*pi);
-            obj.R = eul2rotm(obj.phi);
-            obj.q = rotm2quat(obj.R);
+            if nargin > 1
+                obj.q = angle;
+                obj.R = quat2rotm(angle);
+                obj.phi = rotm2eul(obj.R);
+            else
+                obj.R = quat2rotm(obj.q);
+                obj.phi = rotm2eul(obj.R);
+            end
             rotated = obj.R * [obj.x; obj.y; obj.z];
             obj.x = rotated(1, :);
             obj.y = rotated(2, :);
@@ -100,31 +127,41 @@ classdef Segment
         end
         
         function obj = scale(obj, scalevec)
-            obj.sigma = scalevec;
+            if nargin > 1
+                obj.sigma = scalevec;
+            else
+                scalevec = obj.sigma;
+            end
             obj.x = obj.x * scalevec(1);
             obj.y = obj.y * scalevec(2);
             obj.z = obj.z * scalevec(3);
         end
         
-        function obj = shape(obj, epsilon)
-            obj.epsilon = epsilon;
+        function obj = shape(obj, sz, epsilon)
+            if nargin > 2
+                obj.epsilon = epsilon;
+            else
+                epsilon = obj.epsilon;
+            end
             start = ellipsoidInit(epsilon, 1);
             obj.x = start.XData;
             obj.y = start.YData;
             obj.z = start.ZData;
             clear start
-            obj = obj.scale(obj.sigma).rotate(obj.phi).translate(obj.mu);
+            temp = obj.mu;
+            obj.mu = [0; 0; 0];
+            obj = obj.scale(obj.sigma).rotate(obj.q).translate(temp, sz);
         end
         
         
-        function obj = intensity_est(obj, image, outsideWin)
+        function obj = intensity_est(obj, image, outsideWin, start)
             minr = min(obj.x);
             minc = min(obj.y);
-            minz = min(obj.z);
+            minz = floor(obj.mu(3)) - 1;
             
             maxr = max(obj.x);
             maxc = max(obj.y);
-            maxz = max(obj.z);
+            maxz = floor(obj.mu(3));
             
             [numrow, numcol, numz] = size(image);
             
@@ -143,7 +180,16 @@ classdef Segment
             insideRegion = image(lr:ur, lc:uc, lz:uz);
             
             % Estimate foreground intensity
-            obj.IF = median(insideRegion(:));
+            if start
+                obj.IF0 = median(insideRegion(:));
+                obj.IF = obj.IF0;
+            else
+                % Use skipped median M-estimator for future estimates
+                temp = insideRegion(:) - obj.IF0;
+                s = 0.5 * abs(obj.IF0 - obj.IB0);
+                temp = temp(temp <= s);
+                obj.IF = median(temp);
+            end
             
             % Calculate MAD statistic
             obj.alpha = mad(double(insideRegion(:)));
@@ -161,17 +207,31 @@ classdef Segment
             outsideRegion = image(lr:ur, lc:uc, lz:uz);
             
             % Estimate background intensity
-            obj.IB = median(outsideRegion(:));
+            if true % start
+                obj.IB0 = median(outsideRegion(:));
+                obj.IB = obj.IB0;
+            else
+                % Use skipped median M-estimator for future estimates
+                temp = insideRegion(:) - obj.IB0;
+                s = 0.5 * abs(obj.IF0 - obj.IB0);
+                temp = temp(temp <= s);
+                obj.IB = median(temp);
+            end
         end
         
-        function delL = Lgrad(obj, image)
-            hull = convhull(obj.x, obj.y, obj.z, 'Simplify', true);
+        function [delL, L] = Lgrad(obj, image, start)
+            try
+                hull = convhull(obj.x, obj.y, obj.z, 'Simplify', true);
+            catch EXCEPT
+                delL = zeros(11, 3);
+                L = NaN;
+                return
+            end
             set(0,'DefaultFigureVisible','off');
             t = trisurf(hull, obj.x, obj.y, obj.z);
             set(0,'DefaultFigureVisible','on');
-            np = reducepatch(t, 0.2);
+            np = reducepatch(t, 0.05);
             [numrow, numcol, numz] = size(image);
-            delL = 0;
             
             Aind = np.faces(:, 1);
             Bind = np.faces(:, 2);
@@ -200,53 +260,120 @@ classdef Segment
             
             IC = image(li);
             aVec = cross(Bv - Av, Cv - Av);
-            lens = sqrt(sum(abs(aVec).^2,2));
+            if any(isnan(aVec))
+                aVec = cross(Av - Bv, Av - Cv);
+            end
+            lens = sqrt(sum(abs(aVec).^2,2)) + 0.001;
             surfnorms = aVec ./ lens;
             areas = 0.5 * lens;
             
             F = log(obj.f(IC - obj.IF) ./ obj.f(IC - obj.IB));
-            F(isinf(F)) = 0;
-            J = permute(obj.Jacobian(true)', [1, 2, 3]);
-            J = repmat(J, 1, 1, length(F));
-            temp = (F .* areas .* surfnorms)';
-            temp = permute(temp, [1, 3, 2]);
-            delL = sum(mtimesx(J, temp, 'LOOPS'), 3);
-%             for f = 1:size(np.faces, 1)
-%                 v1 = np.vertices(np.faces(f, 1), :);
-%                 v2 = np.vertices(np.faces(f, 2), :);
-%                 v3 = np.vertices(np.faces(f, 3), :);
-%                 
-%                 c = int64(floor(mean([v1; v2; v3], 1)));
-%                 if c(1) < 1
-%                     c(1) = 1;
-%                 elseif c(1) > numrow
-%                     c(1) = numrow;
-%                 end
-%                 if c(2) < 1
-%                     c(2) = 1;
-%                 elseif c(2) > numcol
-%                     c(2) = numcol;
-%                 end
-%                 if c(3) < 1
-%                     c(3) = 1;
-%                 elseif c(3) > numz
-%                     c(3) = numz;
-%                 end
-%                 IC = image(c(1), c(2), c(3));
-%                 
-%                 aVec = cross(v2 - v1, v3 - v1)';
-%                 surfNorm = aVec / norm(aVec);
-%                 surfaceArea = 0.5 * norm(aVec);
-%                 
-%                 F = log(obj.f(IC - obj.IF) ./ obj.f(IC - obj.IB));
-%                 if isinf(F)
-%                     F = 0;
-%                 end
-%                 J = obj.Jacobian(true);
-%                 
-%                 delL = delL + F * J' * surfNorm * surfaceArea;
-%                 
-%             end
+            F(isinf(F)) = -5000;
+            if start
+                J = permute(obj.Jacobian(true)', [1, 2, 3]);
+                J = repmat(J, 1, 1, length(F));
+                temp = (F .* areas .* surfnorms)';
+                temp = permute(temp, [1, 3, 2]);
+                delL = sum(mtimesx(J, temp, 'SPEEDOMP'), 3);
+                if any(isnan(delL))
+                    delL(isnan(delL)) = 0;
+                end
+                L = sum(F);
+            else
+                J = NaN(11, 3, length(F));
+                for i = 1:size(c, 1)
+                    J(:, :, i) = obj.Jacobian(false, c(i, :))';
+                end
+                temp = (F .* areas .* surfnorms)';
+                temp = permute(temp, [1, 3, 2]);
+                delL = sum(mtimesx(J, temp, 'SPEEDOMP'), 3);
+                L = sum(F);
+            end
+        end
+        
+        function L = likelihood(obj, image)
+            minr = min(obj.x);
+            minc = min(obj.y);
+            minz = floor(obj.mu(3)) - 1;
+            
+            maxr = max(obj.x);
+            maxc = max(obj.y);
+            maxz = floor(obj.mu(3));
+            
+            [numrow, numcol, numz] = size(image);
+            
+            
+            lr = floor(max([1, minr]));
+            ur = floor(min([maxr, numrow]));
+            
+            lc = floor(max([1, minc]));
+            uc = floor(min([maxc, numcol]));
+            
+            lz = floor(max([1, minz]));
+            uz = floor(min([maxz, numz]));
+            
+            % Get bounding box for ellipsoid (easier/more efficient than convex
+            % hull)
+            insideRegion = image(lr:ur, lc:uc, lz:uz);
+            insideRegion = insideRegion(:);
+            in = abs(insideRegion - obj.IF);
+            out = abs(insideRegion - obj.IB);
+            L = (1/obj.alpha) * sum(-in + out);
+        end
+        
+        function [newMask, newCol, newColPoints, done] = ...
+                markVisited(obj, visited, mask, label, collisions, pts)
+            done = false;
+            newCol = collisions;
+            newColPoints = pts;
+            minr = min(obj.x);
+            minc = min(obj.y);
+            minz = floor(obj.mu(3)) - 1;
+            
+            maxr = max(obj.x);
+            maxc = max(obj.y);
+            maxz = floor(obj.mu(3));
+            
+            [numrow, numcol, numz] = size(visited);
+            
+            
+            lr = floor(max([1, minr]));
+            ur = floor(min([maxr, numrow]));
+            
+            lc = floor(max([1, minc]));
+            uc = floor(min([maxc, numcol]));
+            
+            lz = floor(max([1, minz]));
+            uz = floor(min([maxz, numz]));
+            
+            roi = visited(floor(lr):floor(ur), floor(lc):floor(uc), ...
+                floor(lz):floor(uz));
+            if any((roi ~= 0) & (roi ~= label))
+                ind = find((roi ~= 0) & (roi ~= label), 1);
+                newCol = vertcat(collisions, [label, roi(ind)]);
+                newColPoints = vertcat(pts, obj);
+                done = true;
+            end
+            if lr == minr || ur == numrow || lc == minc || uc == numcol ||...
+                    lz == minz || uz == numz
+                done = true;
+            end
+            visited(floor(lr):floor(ur), floor(lc):floor(uc), ...
+                floor(lz):floor(uz)) = label;
+            
+            newMask = visited;
+        end
+        
+        function offVessel = tstat(obj, image, thresh)
+            offVessel = false;
+            try
+                [~, vol] = convhull(obj.x, obj.y, obj.z, 'Simplify', true);
+            catch EXCEPT
+                return
+            end
+            if obj.likelihood(image) / vol < thresh
+               offVessel = true;
+            end
         end
     end
 end
--- vesselTrace.m
+++ vesselTrace.m
@@ -1,44 +1,226 @@
 function vesselTrace(FinalImage, seedCoord)
 
+dt = 0.003;
+
 startEllipse = ellipsoidInit(0.25, 3);
 x = startEllipse.XData;
 y = startEllipse.YData;
 z = startEllipse.ZData;
 
-startSeg = Segment();
-startSeg.x = x;
-startSeg.y = y;
-startSeg.z = z;
+startSeg = Segment(x, y, z);
 
 [numrow, numcol, numz] = size(FinalImage);
 outsideWin = 30;
 
-newSeeds = zeros(size(seedCoord, 1), 3);
+% % Use subset of all points for demo
+% testSeeds = seedCoord; % (seedCoord(:, 3) >= 20 & seedCoord(:, 3) <= 30);
+%
+% newSeeds = NaN(size(testSeeds, 1), 3);
+% L_list = NaN(size(testSeeds, 1), 1);
+% seg_list = startSeg;
+% seg_list = repmat(seg_list, length(L_list), 1);
+%
+% for i = 1:length(seg_list)
+%     point = seedCoord(i, :)';
+%
+%     % Generate temporary segment
+%     temp = seg_list(i);
+%     temp = temp.translate(point, size(FinalImage));
+%     temp = temp.intensity_est(FinalImage, outsideWin, true);
+%     if temp.IB == temp.IF
+%         continue
+%     end
+%     seg_list(i) = temp;
+%     newSeeds(i, :) = temp.mu';
+% end
+%
+% cond = isnan(newSeeds);
+%
+% seg_list(cond(:, 1)) = [];
+% L_list(cond(:, 1)) = [];
+% newSeeds = NaN(size(L_list, 1), 3);
+epsilon = 1e-4;
+% dt = 0.003;
+% % Update position
+% for i = 1:length(seg_list)
+%     prevL = zeros(11, 1);
+%     temp = seg_list(i);
+%     for iter = 1:20
+%         % Next estimate new mu
+%         % First calculate likelihood gradient
+%         [delL, L] = temp.Lgrad(FinalImage, true);
+%         % Check for convergence
+%         if all(abs(delL - prevL) < epsilon)
+%             break
+%         end
+%         delL = delL * dt;
+%         prevL = delL;
+%         temp.L = L;
+%         muN = delL(1:3);
+%         temp = temp.translate(muN, [numrow, numcol, numz]);
+%         newSeeds(i, :) = temp.mu';
+%         L_list(i) = L;
+%         seg_list(i) = temp;
+%         if any(isinf(delL) | isnan(delL))
+%             break
+%         end
+%     end
+% end
+%
+% newSeeds(:, 3) = floor(newSeeds(:, 3));
+%
+% % Save so if we ever get here, we don't have to do it again
+% save('newSeeds5.mat', 'newSeeds', 'seg_list', 'L_list')
 
-for i = 1:size(seedCoord, 1)
-    point = seedCoord(i, :)';
-    
-    % Generate temporary segment
-    temp = startSeg;
-    temp = temp.translate(point);
-    temp = temp.intensity_est(FinalImage, outsideWin);
-    
-    % Next estimate new mu
-    % First calculate likelihood gradient
-    delL = temp.Lgrad(FinalImage) * 0.03;
-    muN = delL(1:3);
-    temp = temp.translate(muN);
-    temp.mu(1) = temp.mu(1) * (temp.mu(1) >= 1 && temp.mu(1) <= numrow) + ...
-        (temp.mu(1) < 1) + (temp.mu(1) > numrow) * numrow;
-    temp.mu(2) = temp.mu(2) * (temp.mu(2) >= 1 && temp.mu(2) <= numcol) + ...
-        (temp.mu(2) < 1) + (temp.mu(2) > numcol) * numcol;
-    temp.mu(3) = temp.mu(3) * (temp.mu(3) >= 1 && temp.mu(3) <= numz) + ...
-        (temp.mu(3) < 1) + (temp.mu(3) > numz) * numz;
-    
-    newSeeds(i, :) = temp.mu';
-    
+% load('newSeeds5.mat')
+% Now reestimate other parameters
+% dt = 0.01;
+% maxIt = 20;
+% tic;
+% for i = 1:length(seg_list)
+%     if i == 265
+%         continue
+%     end
+%     prevL = zeros(11, 1);
+%     temp = seg_list(i);
+%     display(i)
+%     toc;
+%     for iter = 1:maxIt
+%         if mod(i, 5) == 0
+%             temp = temp.intensity_est(FinalImage, outsideWin, false);
+%         end
+%         [delL, L] = temp.Lgrad(FinalImage, false);
+%         Check for convergence
+%         if all(abs(delL - prevL) < epsilon)
+%             break
+%         end
+%         delL = delL * dt;
+%         if any(delL >= 1e6)
+%             break
+%         end
+%         if ~isnan(L)
+%             temp.L = L;
+%         else
+%             L = L_list(i);
+%         end
+%         temp.sigma = temp.sigma + delL(4:6);
+%         temp.q = temp.q + delL(7:10)';
+%         temp.epsilon = temp.epsilon + delL(11);
+%         Shape method applies all other changes to starting ellipsoid
+%         temp = temp.shape(size(FinalImage));
+%         L_list(i) = L;
+%         seg_list(i) = temp;
+%     end
+% end
+%
+% save('newSeeds6.mat', 'L_list', 'seg_list')
+
+%%
+load('newSeeds6.mat')
+for i = 1:length(seg_list)
+    L_list(i) = seg_list(i).IB - seg_list(i).IF;
 end
+[~, inds] = sort(L_list);
+candidates = seg_list(inds(end:-1:1));
+L_list = L_list(inds(end:-1:1));
+inds = [];
+for i = 1:length(candidates)
+    if isempty(candidates(i).x) || any(candidates(i).mu([1, 2]) < 4) ||...
+            candidates(i).mu(1) > numrow - 3 ...
+            || candidates(i).mu(2) > numcol - 3 % ||candidates(i).mu(3) == numz
+        inds = horzcat(inds, i);
+    end
+end
+candidates(inds) = [];
+[XX, YY, ZZ] = meshgrid(1:numrow, 1:numcol, 1:numz);
+visited = zeros(size(FinalImage));
 
-save('newSeeds.mat', 'newSeeds')
 
+numToVisit = 1;
+vessels = cell(1, numToVisit);
+collisions = [];
+collPoints = [];
+done = false;
+
+maxIt = 30;
+updateIt = 10;
+for v = 5
+    cur = candidates(v);
+    [visited, collisions, collPoints, done] = cur.markVisited(visited,...
+        {XX, YY, ZZ}, v, collisions, collPoints);
+    % Make segment container
+    segs = cell(1, 2 * maxIt + 1);
+    segs{maxIt + 1} = cur;
+    evecs = pca([cur.x', cur.y', cur.z']);
+    % Should be largest eigenvector
+    p_axis = evecs(:, 1);
+    % Make unit length
+    p_axis = p_axis / norm(p_axis);
+    
+    choices = [-p_axis, p_axis];
+    for c = 1:2
+        prevDir = choices(:, c);
+        prevSeg = cur;
+        for i = 1:maxIt
+            newSeg = prevSeg;
+            newSeg = newSeg.translate(prevDir, size(FinalImage));
+            evecs = pca([newSeg.x', newSeg.y', newSeg.z']);
+            newDir = evecs(:, 1) / norm(evecs(:, 1));
+            angle = atan2(norm(cross(prevDir,newDir)),dot(prevDir,newDir));
+            if angle > pi/2
+                newDir = -newDir;
+            end
+            prevDir = newDir;
+            prevL = zeros(11, 1);
+            for it = 1:updateIt
+                [delL, ~] = newSeg.Lgrad(FinalImage, false);
+                % Check for convergence
+                if all(abs(delL - prevL) < epsilon)
+                    break
+                end
+                delL = delL * dt;
+                if any(delL >= 1e6)
+                    break
+                end
+                newSeg.sigma = newSeg.sigma + delL(4:6);
+                newSeg.q = newSeg.q + delL(7:10)';
+                newSeg.epsilon = newSeg.epsilon + delL(11);
+                % Shape method applies all other changes to starting ellipsoid
+                newSeg = newSeg.shape(size(FinalImage));
+                prevL = delL;
+                newSeg = newSeg.intensity_est(FinalImage, outsideWin, false);
+            end
+            
+            
+            [visited, collisions, collPoints, done] =...
+                newSeg.markVisited(visited, {XX, YY, ZZ}, v, collisions, collPoints);
+            
+            if done || newSeg.tstat(FinalImage, 0.2)
+                break
+            end
+            
+            if c == 1
+                segs{maxIt + 1 + i} = newSeg;
+            else
+                segs{maxIt + 1 - i} = newSeg;
+            end
+            
+            prevSeg = newSeg;
+        end
+    end
+    vessels{v} = segs;
+    mip = min(FinalImage, [], 3);
+    points = [];
+    for i = 1:length(segs)
+        if ~isempty(segs{i})
+            points = vertcat(points, segs{i}.mu');
+        end
+    end
+    figure(1)
+    plot(points(:, 1), points(:, 2))
+    imshow(mip)
+    hold on
+    plot(points(:, 2), points(:, 1))
+    hold off
+end
 end
